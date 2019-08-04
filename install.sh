0a528290c0a103c265b521b8d3932fa9c5b00941
927589452 <>
2019-08-04 23:02:47 +0200

    move to user directory and link the scripts, so future updates don't need to install again'

--- install.sh
+++ install.sh
@@ -1,7 +1,23 @@
-#!/bin/bash
+#!/bin/sh
 
-# Run as root, this will add the scripts to your path
+# Set a path for local binaries
+# add this to your path
+BINPATH=$HOME/.local/bin
 
-chmod +x *
-cp ./genzathurarc /usr/bin/
-cp ./zth /usr/bin/
+# make sure this path exists
+mkdir -p "$BINPATH"
+
+# make the scripts executable
+chmod u+x ./genzathurarc
+chmod u+x ./zth
+
+# link the scripts to the PATH
+ln -s "$(pwd)/genzathurarc" "$BINPATH/genzathurarc"
+ln -s "$(pwd)/zth" "$BINPATH/zth"
+
+cat <<EOM
+Installed to "$BINPATH"
+Make sure "$BINPATH" is in your PATH
+you may do so by adding the following line to your bashrc or zshrc
+export PATH="$BINPATH:\$PATH"
+EOM
