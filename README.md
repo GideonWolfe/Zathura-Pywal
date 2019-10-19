7388221e0f522633d25c9a44477850fb22d2d771
Adrian <adrian.salas@protonmail.com>
2019-10-19 07:36:19 +0200

    improved the script by making use of the template feature

--- README.md
+++ README.md
@@ -7,6 +7,10 @@ Zathura-Pywal is a set of helper scripts that dynamically theme zathura based on
 
 ## Installation
 
-To install, simply move into the clone directory and run `./install.sh`
+To install, simply move into the clone directory and run `./install.sh`.
+The script is going to create a template for pywal that will generate a config file for zathura at `~/.cache/wal`.
+In order to open zathura with the generated colorscheme you need to open your file with `zathura --config-dir "~/.cache/wal/"`, dont put the name of the config file, zathura is gonna find it himself :).
+
+Ps. just make an alias in yout .bashrc or .zshrc file for easier access
 
 You can toggle normal colors by pressing `Ctrl+r`
--- install.sh
+++ install.sh
@@ -1,23 +1,19 @@
 #!/bin/sh
 
-# Set a path for local binaries
+# Set a path for the pywal template config file
 # add this to your path
-BINPATH=$HOME/.local/bin
-
+TEMPLATEPATH=$HOME/.config/wal/templates
+CACHEPATH=$HOME/.cache/wal/
 # make sure this path exists
-mkdir -p "$BINPATH"
+mkdir -p "$TEMPLATEPATH"
 
 # make the scripts executable
 chmod u+x ./genzathurarc
-chmod u+x ./zathura
-
-# link the scripts to the PATH
-ln -s "$(pwd)/genzathurarc" "$BINPATH/genzathurarc"
-ln -s "$(pwd)/zathura" "$BINPATH/zathura"
+touch "$TEMPLATEPATH"/zathurarc
+genzathurarc >> "$TEMPLATEPATH"/zathurarc
 
 cat <<EOM
-Installed to "$BINPATH"
-Make sure "$BINPATH" is in your PATH
-you may do so by adding the following line to your bashrc or zshrc
-export PATH="$BINPATH:\$PATH"
+created a template to "$TEMPLATEPATH"
+Pywal is going to generate a config file for zathura located at "$HOME/.cache/wal/", 
+to open a file on zathura with the new colorsccheme make sure to open your file with "zathura --config-dir "$CACHEPATH" " or just make an alias in your ~/.bashrc or ~/.zshrc
 EOM
